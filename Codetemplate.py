98bfdc8f0583ad75707482ff73a157548eaf3133
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-03 21:37:58 +0500

    fixed the python version

---
+++ Codetemplate.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    # MODIFY THIS PART based on problem:
+    
+    # If single number:
+    n = int(input())
+    
+    # If array:
+    # arr = list(map(int, input().split()))
+    
+    # If two numbers:
+    # a, b = map(int, input().split())
+    
+    # Your logic here
+    
+    # print(result)
+
+if __name__ == "__main__":
+    solve()
