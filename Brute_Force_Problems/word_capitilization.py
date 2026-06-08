469196984f1bfaabe65265f2db7dff8a6f44f9a8
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-08 10:07:31 +0500

    added the problem statement

--- Brute_Force_Problems/word_capitilization.py
+++ Brute_Force_Problems/word_capitilization.py
@@ -1,6 +1,32 @@
 import sys
 input = sys.stdin.readline
 
+"""
+Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
+
+Note, that during capitalization all the letters except the first one remains unchanged.
+
+Input
+A single line contains a non-empty word. This word consists of lowercase and uppercase English letters. The length of the word will not exceed 103.
+
+Output
+Output the given word after capitalization.
+
+Examples
+
+Input
+ApPLe
+
+Output
+ApPLe
+
+Input
+konjac
+
+Output
+Konjac
+"""
+
 def solve():
     word = list(input().strip())
     word[0]= word[0].upper()
