9c4242bcfd2751bf71cbcf1b061b6838d4f0a491
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-09 14:41:53 +0500

    added the problem statement

--- Brute_Force_Problems/elephant.py
+++ Brute_Force_Problems/elephant.py
@@ -2,6 +2,30 @@ import sys
 input = sys.stdin.readline
 import math
 
+"""
+An elephant decided to visit his friend. It turned out that the elephant's house is located at point 0 and his friend's house is located at point x(x>0) of the coordinate line. In one step the elephant can move 1, 2, 3, 4 or 5 positions forward. Determine, what is the minimum number of steps he need to make in order to get to his friend's house.
+
+Input
+The first line of the input contains an integer x (1≤x≤1000000) — The coordinate of the friend's house.
+
+Output
+Print the minimum number of steps that elephant needs to make to get from point 0 to point x.
+
+Examples
+
+Input
+5
+
+Output
+1
+
+Input
+12
+
+Output
+3
+"""
+
 def solve():
     n = int(input())
     steps = math.ceil(n/5)
