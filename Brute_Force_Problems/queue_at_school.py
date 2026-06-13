6b9392de000576d51196ef5860ad9e1e9f7bf1d0
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-13 17:34:01 +0500

    solved the problem 266B

---
+++ Brute_Force_Problems/queue_at_school.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    n, t = map(int, input().split())
+    queue = list(input().strip())
+
+    for _ in range(t):
+        i = 0
+        while i < n - 1:
+            if queue[i] == "B" and queue[i + 1] == "G":
+                queue[i], queue[i + 1] = queue[i + 1], queue[i]
+                i += 2   
+            else:
+                i += 1
+
+    print("".join(queue))
+
+if __name__ == "__main__":
+    solve()
