d8b394b3d99a577ce1ae54d7f5f84e123850a2bc
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-17 08:36:23 +0500

    added the problem statement

--- Brute_Force_Problems/a+b(again).py
+++ Brute_Force_Problems/a+b(again).py
@@ -1,6 +1,41 @@
 import sys
 input = sys.stdin.readline
 
+"""
+Given a two-digit positive integer n, find the sum of its digits.
+
+Input
+The first line contains an integer t(1≤t≤90) — the number of test cases.
+
+The only line of each test case contains a single two-digit positive integer n(10≤n≤99).
+
+Output
+For each test case, output a single integer — the sum of the digits of n.
+
+Example
+
+Input
+8
+77
+21
+40
+34
+19
+84
+10
+99
+
+Output
+14
+3
+4
+7
+10
+12
+1
+18
+"""
+
 def solve():
     n = int(input())
 
