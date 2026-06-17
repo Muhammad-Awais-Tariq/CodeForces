76cff4de65f9f8655b70e113fbd974ae2ade5296
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-17 08:47:11 +0500

    solved the problem 705A

---
+++ Brute_Force_Problems/hulk.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    n = int(input())
+    for i in range(1 , n+1):
+        if i % 2 != 0:
+            print("I hate" , end= " ")
+        else:
+            print("I love" , end= " ")
+        if i != n:
+            print("that" , end=" ")
+        else:
+            print(" it")
+        
+
+
+if __name__ == "__main__":
+    solve()
