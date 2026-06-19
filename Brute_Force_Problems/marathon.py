38406f95145e64ff4c4954e921c810a853d072a4
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-19 15:40:26 +0500

    added the problem statement

--- Brute_Force_Problems/marathon.py
+++ Brute_Force_Problems/marathon.py
@@ -1,6 +1,37 @@
 import sys
 input = sys.stdin.readline
 
+"""
+You are given four distinct integers a, b, c, d.
+
+Timur and three other people are running a marathon. The value a is the distance that Timur has run and b, c, d correspond to the distances the other three participants ran.
+
+Output the number of participants in front of Timur.
+
+Input
+The first line contains a single integer t (1≤t≤104) — the number of test cases.
+
+The description of each test case consists of four distinct integers a, b, c, d (0≤a,b,c,d≤104).
+
+Output
+For each test case, output a single integer — the number of participants in front of Timur.
+
+Example
+
+Input
+4
+2 3 4 1
+10000 0 1 2
+500 600 400 300
+0 9999 10000 9998
+
+Output
+2
+0
+1
+3
+"""
+
 def solve():
     n = int(input())
 
