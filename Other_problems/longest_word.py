1ae4f8c6ffaf86704ea0dc934032931d9560615b
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-23 11:12:52 +0500

    added the problem statement

--- Other_problems/longest_word.py
+++ Other_problems/longest_word.py
@@ -1,3 +1,26 @@
+
+"""
+Given a sentence containing words separated by spaces, find the longest word.
+
+If multiple words have the same maximum length, print the first one.
+
+Input
+
+A single line containing a sentence.
+
+Output
+
+Print the longest word.
+
+Example 
+
+Input
+python is awesome
+
+Output
+awesome
+"""
+
 sentence = input().split()
 max_len = 0
 max_word = ""
