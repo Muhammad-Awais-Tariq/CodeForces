8ed6ad0fdaa1c6a7302fdf6a8d54269ba9c9a1e0
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-23 11:00:42 +0500

    added the problem statement

--- Other_problems/consective_difference.py
+++ Other_problems/consective_difference.py
@@ -1,3 +1,26 @@
+
+"""
+Given a list of integers, determine whether the difference between every pair of consecutive elements is exactly 1.
+
+Print "YES" if true, otherwise print "NO".
+
+Input
+First line: integer n
+Second line: n space-separated integers
+Output
+
+Print YES or NO.
+
+Example 
+Input
+
+5
+3 4 5 6 7
+Output
+
+YES
+"""
+
 n = int(input())
 array = list(map(int , input().split()))
 i = 0
