fb5ef548f160fa366924a0ece84dff183eb5772e
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-24 09:12:30 +0500

    added the problem statement

--- Other_problems/second_largest.py
+++ Other_problems/second_largest.py
@@ -1,3 +1,16 @@
+
+"""
+Task:
+Given n integers, find the second largest distinct number.
+
+Input
+5
+10 20 30 40 50
+
+Output
+40
+"""
+
 numbers = list(set(map(int , input().split())))
 numbers.sort(reverse=True)
 print(numbers[1])
