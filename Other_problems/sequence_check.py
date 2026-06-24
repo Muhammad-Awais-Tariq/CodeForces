9550cbeb8fbb222649bab2fe3d69c0796893514c
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-24 09:36:00 +0500

    added the problem statement

--- Other_problems/sequence_check.py
+++ Other_problems/sequence_check.py
@@ -1,4 +1,22 @@
 
+"""
+Given an array of integers, determine whether every adjacent pair differs by exactly 1.
+
+Input
+5
+3 4 5 6 7
+
+Output
+YES
+
+Input
+5
+3 4 6 7 8
+
+Output
+NO
+"""
+
 total_num = int(input())
 numbers = list(map(int , input().split()))
 consecutive = True
