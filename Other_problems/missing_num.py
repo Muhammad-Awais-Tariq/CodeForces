10d422c76ed1a52d22385f686cdf5b61e5cfd678
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-25 08:55:32 +0500

    added the problem statement

--- Other_problems/missing_num.py
+++ Other_problems/missing_num.py
@@ -1,3 +1,18 @@
+
+"""
+Find the Missing Number
+
+You are given n-1 distinct numbers from 1 to n. One number is missing. Find it.
+
+Example
+Input:
+5
+1 2 4 5
+
+Output:
+3
+"""
+
 n = int(input())
 numbers = list(map(int , input().split()))
 all_numbers_sum = sum([i for i in range(1,n+1)])
