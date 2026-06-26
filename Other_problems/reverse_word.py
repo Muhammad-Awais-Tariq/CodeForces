beb7efd094855fd66129ff1d592c79b7d157a6d2
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-26 08:16:34 +0500

    added the problem statement

--- Other_problems/reverse_word.py
+++ Other_problems/reverse_word.py
@@ -1,3 +1,20 @@
+
+"""
+Problem: Reverse Each Word
+
+Write a program that:
+
+Takes a sentence as input.
+Reverses each word individually while keeping the word order the same.
+
+Example
+Input
+hello python world
+
+Output
+olleh nohtyp dlrow
+"""
+
 words = input().split()
 revers_words = []
 
