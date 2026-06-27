27e5b0115b863d70472f3413a9eb51eb62d6e913
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-27 08:17:37 +0500

    added the problem statement

--- Other_problems/matrix_border_sum.py
+++ Other_problems/matrix_border_sum.py
@@ -1,4 +1,26 @@
 
+"""
+Matrix Border Sum
+
+Given an n x n matrix, calculate the sum of only the border elements.
+
+Input
+3
+1 2 3
+4 5 6
+7 8 9
+
+Output
+40
+
+Explanation:
+1 2 3
+4   6
+7 8 9
+
+Border sum = 1+2+3+4+6+7+8+9 = 40
+"""
+
 n = int(input())
 matrix = []
 
